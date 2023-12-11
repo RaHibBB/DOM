42f97b34fcf136f0ea5225bcfc60278f5b748590
RaHibBB <mdrahibchowdhury@gmail.com>
2023-12-11 18:24:35 +0600

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+const blogSection = document.getElementById('blog-section');
+blogSection.style.padding = "50px 10px 20px 30px";
+
+document.getElementsByClassName('heading').style.fontSize = "32px";
+alert("Hello! I am an alert box!!");
+
