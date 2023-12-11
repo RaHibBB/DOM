5770596ee507fd7567b9f487420766d7972018b8
RaHibBB <mdrahibchowdhury@gmail.com>
2023-12-11 21:09:48 +0600

    4th

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const blogSection = document.getElementsByClassName('blog-area');
-blogSection.style.padding = "100px 10px 20px 30px";
-blogSection.style.display = 'flex'
+const headingArea = document.getElementById('heading');
+headingArea.style.textAlign = 'center';
+headingArea.style.marginTop = '100px';
+headingArea.style.fontSize = '42px';
+const sec = document.getElementById('blog-area');
+sec.style.padding = '50px 0px 100px 0px';
+sec.style.display = 'flex';
+sec.style.gap = '30px';
+sec.style.justifyContent ='center';
+sec.style.maxWidth = '1250px';
+sec.style.margin = 'auto';
 
-const image = document.getElementsByTagName('img');
-image.style.width = '300px';
 
-const headingText = document.getElementsByClassName('heading')
-headingText.style.fontSize = '32px';
-headingText.style.color = 'purple'
+const image = document.getElementsByClassName('imgg');
+for(const singleImage of image){
+    singleImage.style.width = '300px';
+  
+}
+const blogCard = document.getElementsByClassName('blog');
+for(const blogItem of blogCard){
+    blogItem.style.display= 'flex';
+    blogItem.style.flexDirection ='column';
+    blogItem.style.backgroundColor = 'purple';
+    blogItem.style.textAlign = 'center';
+    blogItem.style.alignItems = 'center';
+    blogItem.style.padding = '20px';
+    blogItem.style.color = '#fff';
+    blogItem.style.border = '5px solid pink';
+    blogItem.style.borderRadius = '14px';
+}
+// document.getElementsByTagName('img').style.width = '300px'
 
 
