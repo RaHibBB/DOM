ac1737d7a0231ad0accc56477f0a52e70ce4c0e6
RaHibBB <mdrahibchowdhury@gmail.com>
2023-12-11 22:43:44 +0600

    responsive

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ sec.style.justifyContent ='center';
 sec.style.maxWidth = '1250px';
 sec.style.margin = 'auto';
 
+// Add a media query for screens with a maximum width of 767px (typical for mobile devices)
+const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+// Function to apply styles for mobile screens
+function handleMobileScreen(e) {
+    if (e.matches) {
+        sec.style.flexDirection = 'column'; // Stack elements vertically
+        sec.style.alignItems = 'center'; // Center items in the container
+    } else {
+        sec.style.flexDirection = 'row'; // Reset to horizontal layout for larger screens
+        sec.style.alignItems = 'initial'; // Reset alignment for larger screens
+    }
+}
+
+// Initial check and call the function
+handleMobileScreen(mediaQuery);
+
+// Add listener for changes in screen width
+mediaQuery.addListener(handleMobileScreen);
+
 
 const image = document.getElementsByClassName('imgg');
 for(const singleImage of image){
